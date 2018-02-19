c063a38f20ad60984568bdf81e6f4ed63f9d6382
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-19 15:09:09 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,12 +1,12 @@
 #!groovy 
-def call(body)
- 	{
- 	def config = [:]
- 	body.resolveStrategy = Closure.DELEGATE_FIRST
- 	body.delegate = config
- 	body()
- 	timestamps {
- 	try {
+def call(body)
+  {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+     timestamps {
+       try {
         stage("Code Compile") { 
         print "checkout"
         git 'https://github.com/shekharshamra/jenkin.git'
