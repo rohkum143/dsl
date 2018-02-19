aba2a0a15d30f89af31d201795645bde5fc8f887
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-19 15:14:13 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,10 +1,10 @@
 #!groovy
 def call(body)
   {
-#    def config = [:]
-#    body.resolveStrategy = Closure.DELEGATE_FIRST
-#    body.delegate = config
-#    body()
+//   def config = [:]
+//   body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//    body()
     timestamps {
      try {
         stage("Code Compile") { 
