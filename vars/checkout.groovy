7842652c126eae8da7cf40a9d1fa43363b111716
root <root@delvmplamciserver.sapient.com>
2018-02-19 14:57:38 +0530

    add

---
+++ vars/checkout.groovy
@@ -0,0 +1,18 @@
+def call(body)
+ 	{
+ 	def config = [:]
+ 	body.resolveStrategy = Closure.DELEGATE_FIRST
+ 	body.delegate = config
+ 	body()
+ 	timestamps {
+ 	try {
+        stage("Code Compile") { 
+        print "checkout"
+        git 'https://github.com/shekharshamra/jenkin.git'
+                  }
+       }
+        catch (Exception caughtExp) { 
+        print "\u001B[41m[ERROR]: Cargill Brazill CFData pipeline failed, check detailed logs..." 
+        }
+  } 
+}
