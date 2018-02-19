1bdc39c43befd36fd82b5d5cf51a1bebcea25cc0
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-19 15:23:44 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,19 +1,19 @@
 #!groovy
-def call(body)
-  {
-//   def config = [:]
-//   body.resolveStrategy = Closure.DELEGATE_FIRST
-//    body.delegate = config
-//    body()
-    timestamps {
-     try {
-        stage("Code Compile") { 
+def call(body)
+       {
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+        timestamps {
+        try {
+        stage("Code Compile") {
         print "checkout"
         git 'https://github.com/shekharshamra/jenkin.git'
-        }
+          }
        }
-       catch (Exception caughtExp) { 
-       print "\u001B[41m[ERROR]: check detailed logs..." 
-     }
- } 
+        catch (Exception caughtExp) {
+        print "\u001B[41m[ERROR]: Cargill Brazill CFData pipeline failed, check detailed logs..."
+        }
+  }
 }
