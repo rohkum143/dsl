7cd43532c2890fd799bd038d3f0cc13cce5198e9
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-19 16:08:25 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,22 +1,21 @@
-
-def call(body)
-   {
-        def config = [:]
-        body.resolveStrategy = Closure.DELEGATE_FIRST
-        body.delegate = config
-        body()
-      timestamps {
-         try {
+#!groovy
+def call(body)
+  {
+    def config = [:]
+      body.resolveStrategy = Closure.DELEGATE_FIRST
+      body.delegate = config
+      body()
+       timestamps {
+          try {
                stage("Code Compile") {
                   echo "checkout"
-                  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shekharshamra/jenkin.git']]])
+                  //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shekharshamra/jenkin.git']]])
                 }
             }
         
-		catch (Exception caughtExp) {
-        print "[ERROR]: Cargill Brazill CFData pipeline failed, check detailed logs..."
-        }
-		
-	  }	
+          catch (Exception caughtExp) {
+             print "[ERROR]: Cargill Brazill CFData pipeline failed, check detailed logs..."
+          }
+        }            
      
-    }
+  }
