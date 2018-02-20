f7015c67516dc98d07aefc5444d9352bc873780d
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-20 11:07:13 +0530

    Update git.groovy

--- src/com/test/git.groovy
+++ src/com/test/git.groovy
@@ -1,10 +1,9 @@
 package.com.test
    def CheckOut() {
-                  try {
-                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shekharshamra/jenkin.git']]])
-                      }
-                      catch (Exception caughtExp) {
-                      print "[ERROR]: Cargill Brazill CFData pipeline failed, check detailed logs..."
-                                                   }
-     
-                     }
+             try {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shekharshamra/jenkin.git']]])
+               }
+            catch (Exception caughtExp) {
+           print "[ERROR]: Cargill Brazill CFData pipeline failed, check detailed logs..."
+                                       }
+               }
