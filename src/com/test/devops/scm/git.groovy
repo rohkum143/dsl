dd821ebbc6ce407e118637a4d3a7d31b8d084b21
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-21 11:21:59 +0530

    Update git.groovy

--- src/com/test/devops/scm/git.groovy
+++ src/com/test/devops/scm/git.groovy
@@ -1,8 +1,8 @@
 package com.test.devops.scm
 
-def CheckOut() {
+def CheckOut(String GIT_URL) {
       try {
-            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shekharshamra/jenkin.git']]]
+            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]]
            //git 'https://github.com/shekharshamra/jenkin.git'
             //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/rohkum143/dsl.git']]]).wait()
            }
