95baceff9eae79035306a313db043ac2d3092e91
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-21 10:01:17 +0530

    Update git.groovy

--- src/com/test/devops/scm/git.groovy
+++ src/com/test/devops/scm/git.groovy
@@ -2,9 +2,9 @@ package com.test.devops.scm
 
 def CheckOut() {
       try {
-            //checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shekharshamra/jenkin.git']]]
+            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/shekharshamra/jenkin.git']]]
            //git 'https://github.com/shekharshamra/jenkin.git'
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/rohkum143/dsl.git']]]).wait()
+            //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/rohkum143/dsl.git']]]).wait()
            }
       catch (Exception caughtExp) {
             print " pipeline failed, check detailed log" + caughtExp.getMessage()
