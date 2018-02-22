10fc0f120143551e4b44a0065b913b5bcddc11ed
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-22 13:38:45 +0530

    Update git.groovy

--- src/com/test/devops/scm/git.groovy
+++ src/com/test/devops/scm/git.groovy
@@ -2,8 +2,8 @@ package com.test.devops.scm
 
 def CheckOut(String GIT_URL,String TARGET_DIR) {
       try {
-            //checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]]
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: "${TARGET_DIR}"]], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]])
+            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]]
+            //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: "${TARGET_DIR}"]], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]])
 
            //git 'https://github.com/shekharshamra/jenkin.git'
             //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/rohkum143/dsl.git']]]).wait()
