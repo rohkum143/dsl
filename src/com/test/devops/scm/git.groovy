4b32b6296b43234e34cd5dd975471c3631358f8f
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-22 14:48:03 +0530

    Update git.groovy

--- src/com/test/devops/scm/git.groovy
+++ src/com/test/devops/scm/git.groovy
@@ -3,11 +3,7 @@ package com.test.devops.scm
 def CheckOut(String GIT_URL) {
       try {
             checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]]
-            //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: "${TARGET_DIR}"]], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]])
-
-           //git 'https://github.com/shekharshamra/jenkin.git'
-            //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/rohkum143/dsl.git']]]).wait()
-           }
+              }
       catch (Exception caughtExp) {
             print " pipeline failed, check detailed log" + caughtExp.getMessage()
             currentBuild.result="FAILURE"
@@ -16,7 +12,7 @@ def CheckOut(String GIT_URL) {
 def CodeCompile(String mvnHome) {
       try {
             if (isUnix()) {
-             sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean install"
+                  sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean install -f ${env.Target_DIR}/pom.xml"
              } else {
              bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean install/)
              }
