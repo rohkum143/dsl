fbbc17c83947af017c1aaa83b7462335b0446d16
rohkum143 <rohtashkumar98@yahoo.com>
2018-02-22 12:40:54 +0530

    Update git.groovy

--- src/com/test/devops/scm/git.groovy
+++ src/com/test/devops/scm/git.groovy
@@ -2,7 +2,9 @@ package com.test.devops.scm
 
 def CheckOut(String GIT_URL) {
       try {
-            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]]
+            //checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]]
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CleanBeforeCheckout'], [$class: 'RelativeTargetDirectory', relativeTargetDir: '/app/blue_ocean/jenkins/workspace/DSL-Test']], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]])
+
            //git 'https://github.com/shekharshamra/jenkin.git'
             //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/rohkum143/dsl.git']]]).wait()
            }
