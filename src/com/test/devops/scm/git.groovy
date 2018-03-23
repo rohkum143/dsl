d35568dd2fbae5bb3cfcae9990d582a5247a2a79
rohkum143 <rohtashkumar98@yahoo.com>
2018-03-23 11:48:56 +0530

    Update git.groovy

--- src/com/test/devops/scm/git.groovy
+++ src/com/test/devops/scm/git.groovy
@@ -3,8 +3,6 @@ package com.test.devops.scm
 def CheckOut(String GIT_URL) {
       try {
             checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${GIT_URL}"]]]
-            GIT_COMMIT = getGitCommitHash()
-            echo " ${GIT_COMMIT} "
             }
       catch (Exception caughtExp) {
             print " pipeline failed, check detailed log" + caughtExp.getMessage()
