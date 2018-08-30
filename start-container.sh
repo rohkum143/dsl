9fbec15ab2bb9448537fb09f964a197a5e53e4c2
rohkum143 <rohtashkumar98@yahoo.com>
2018-08-30 10:36:56 +0100

    Update start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -11,17 +11,17 @@ fi
 
 
 # delete old master container and start new master container
-sudo docker rm -f master &> /dev/null
+docker rm -f master &> /dev/null
 echo "start master container..."
-sudo docker run -d -v $PWD:/tmp -t --dns 127.0.0.1 -P --name master -h master.bdp.com -w /root rohtashkumar/hadoop-master &> /dev/null
+docker run -d -v $PWD:/opt -t --dns 127.0.0.1 -P --name master -h master.bdp.com -w /root rohtashkumar/hadoop-master &> /dev/null
 
 # get the IP address of master container
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
 
 # init hive-mysql
 echo "start hive-mysql container..."
-sudo docker rm -f hive-mysql &> /dev/null
-sudo docker run -d -v $PWD:/tmp -t --dns 127.0.0.1 -P --name hive-mysql -h mysql.bdp.com -w /root -e JOIN_IP=$FIRST_IP rohtashkumar/hive-mysq &> /dev/null
+docker rm -f hive-mysql &> /dev/null
+docker run -d -t --dns 127.0.0.1 -P --name hive-mysql -h mysql.bdp.com -w /root -e JOIN_IP=$FIRST_IP rohtashkumar/hive-mysql &> /dev/null
 
 
 # delete old slave containers and start new slave containers
@@ -30,10 +30,10 @@ while [ $i -lt $N ]
 do
 	sudo docker rm -f slave$i &> /dev/null
 	echo "start slave$i container..."
-	sudo docker run -d -t --dns 127.0.0.1 -P --name slave$i -h slave$i.bdp.com -e JOIN_IP=$FIRST_IP rohtashkumar/hadoop-slave &> /dev/null
+	sudo docker run -d -v $PWD:/opt -t --dns 127.0.0.1 -P --name slave$i -h slave$i.bdp.com -e JOIN_IP=$FIRST_IP rohtashkumar/hadoop-slave &> /dev/null
 	((i++))
 done
 
 
 # create a new Bash session in the master container
-sudo docker exec -it master bash
+docker exec -it master bash
