eebb1713d37e9d4d5d07ade8f757390445d18c44
rohkum143 <rohtashkumar98@yahoo.com>
2018-09-11 15:29:34 +0100

    Update start-hadoop.sh

--- hadoop-master/files/start-hadoop.sh
+++ hadoop-master/files/start-hadoop.sh
@@ -4,3 +4,7 @@ $HADOOP_INSTALL/sbin/start-dfs.sh
 
 echo -e "\n"
 $HADOOP_INSTALL/sbin/start-yarn.sh
+ssh slave1.bdp.com '$HADOOP_INSTALL/sbin/start-dfs.sh'
+ssh slave2.bdp.com '$HADOOP_INSTALL/sbin/start-dfs.sh'
+ssh slave1.bdp.com '$HADOOP_INSTALL/sbin/start-yarn.sh'
+ssh slave2.bdp.com '$HADOOP_INSTALL/sbin/start-yarn.sh'
