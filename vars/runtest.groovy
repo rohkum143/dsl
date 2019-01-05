518dd1aa7b311b26a66355b448fec91edd6f2cc0
rohkum143 <rohtashkumar98@yahoo.com>
2019-01-05 09:45:59 +0000

    Update runtest.groovy

--- vars/runtest.groovy
+++ vars/runtest.groovy
@@ -4,9 +4,11 @@ import com.test.devops.scm.*
 
 def call(Map Declare) {
 
-    def archType = deciders.archType
-    String requestJson = readFile("test.json")
-    HashMap configurationFile  = (new HashMap(new groovy.json.JsonSlurperClassic().parseText(requestJson))).asImmutable()
-    println configurationFile
+    //def archType = deciders.archType
+    //String requestJson = readFile("test.json")
+    //HashMap configurationFile  = (new HashMap(new groovy.json.JsonSlurperClassic().parseText(requestJson))).asImmutable()
+    //println configurationFile
+    def a = [ 1,2,3 ]
+    println a 
     
     }
