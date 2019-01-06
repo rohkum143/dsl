6c6115739b2f41e26b8088fb9966b562e77ff881
rohkum143 <rohtashkumar98@yahoo.com>
2019-01-06 12:45:01 +0000

    Update runtest.groovy

--- vars/runtest.groovy
+++ vars/runtest.groovy
@@ -3,13 +3,6 @@
 import com.test.devops.scm.*
 
 def call() {
-
-    //def archType = deciders.archType
-    //String requestJson = readFile("test.json")
-    //HashMap configurationFile  = (new HashMap(new groovy.json.JsonSlurperClassic().parseText(requestJson))).asImmutable()
-    //println configurationFile
-    def a = [ 1,2,3 ]
-    println a 
     
     def propJavaOptions = [:]
     def configFile = 'test.yaml'
